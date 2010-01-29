46470edace4e53a9712dc377a29a4b9dcf017720
Phil Thompson <phil@electricvisions.com>
2010-01-29 07:39:35 +0000

    Forgot to add password confirmation validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,13 @@ class User < ActiveRecord::Base
     c.validate_password_field           = false
   end
 
+  validates_presence_of :name, :message => "Please enter your name."
+  validates_presence_of :email, :message => "Please enter your Email Address."
+  validates_presence_of :password, :on => :create, :message => "Please enter a password."
+
+  validates_confirmation_of :password, :on => :create
+  validates_confirmation_of :password, :on => :update, :allow_nil => true
+
   def email_address_with_name
     "#{name} <#{email}>"
   end
