d9a261e545fca1cd81c0cff704e2a15e3c55f29a
Ian Vaughan <ianrvaughan@gmail.com>
2010-04-26 21:42:20 +0100

    schema updated via rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100425211933) do
+ActiveRecord::Schema.define(:version => 20100426195210) do
 
   create_table "pages", :force => true do |t|
     t.string   "name"
@@ -25,6 +25,10 @@ ActiveRecord::Schema.define(:version => 20100425211933) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "document_file_name"
+    t.string   "document_content_type"
+    t.integer  "document_file_size"
+    t.datetime "document_updated_at"
   end
 
   create_table "users", :force => true do |t|
