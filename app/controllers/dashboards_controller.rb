aa9455e053494ec4b69c7b6616f28e1263a83c57
Hanna Kalinowska <hanna.kalinowska@gmail.com>
2010-05-15 17:56:31 +0100

    compressed dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,6 @@
 class DashboardsController < ApplicationController
   def show
-    @versions = Version.all(:conditions => ['created_at > ?', 1.month.ago], :order => 'created_at DESC')
+    @version_groups = Version.all(:conditions => ['created_at > ?', 1.month.ago], :order => 'created_at DESC').group_by{|version| [version.whodunnit, version.created_at.to_date, version.event]}
     @heading = 'Your Dashboard'
   end
 end
