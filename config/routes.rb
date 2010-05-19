7c926797c2b267bb206dac7dffa7bdf92ff602fa
Hanna Kalinowska <hanna.kalinowska@gmail.com>
2010-05-19 16:15:46 +0100

    redirect user to dashboard after login

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :user_sessions
   map.resources :pages
 
-  map.root :controller => 'pages', :action => 'show', :id => 'home-page'
+  map.root :controller => 'dashboards', :action => 'show'
   map.resource :dashboard, :only => :show
 end
 
