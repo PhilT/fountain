83acf7dbb6ee5da892e111b5f4b6a47f681dc0fc
Phil Thompson <phil@electricvisions.com>
2010-06-17 01:09:01 +0100

    switched to bundler and Rails 2.3.8, added some styling to sorted index page

--- config/environment.rb
+++ config/environment.rb
@@ -1,13 +1,8 @@
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  config.gem 'authlogic'
-  config.gem 'bluecloth'
-  config.gem 'haml'
-  config.gem 'paper_trail'
-  config.gem 'whenever', :lib => false
 
   config.action_controller.session = {
     :session_key => '_fountain_session',
